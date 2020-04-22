050c3e5c1a4500c5401ee9364a1d4b5d83da6260
AlfredoRdgz <alfredordgz98@gmail.com>
2020-04-22 10:18:11 -0500

    Implemented verify & login

--- Frontend/scripts/charts.js
+++ Frontend/scripts/charts.js
@@ -1,43 +1,53 @@
+let dataTemp= [];
+let dataHum= [];
+let dataCon= [];
+let dataLux= [];
+let dataPot= [];
+let dataSod= [];
+let dataCal= [];
+let dataNitr= [];
+
 window.addEventListener('load', () => {
+    loadData();
+    loadMinerals();
     let ctx = document.getElementById('temperatureChart').getContext('2d');
-    createChart(ctx, "Temperatura (°C)", []);
+    createChart(ctx, "Temperatura (°C)", dataTemp);
     ctx = document.getElementById('humidityChart').getContext('2d');
-    createChart(ctx, "Humedad (%)", []);
+    createChart(ctx, "Humedad (%)", dataHum);
     ctx = document.getElementById('conductivityChart').getContext('2d');
-    createChart(ctx, "Conductividad (S/cm)", []);
+    createChart(ctx, "Conductividad (S/cm)", dataCon);
     ctx = document.getElementById('luxChart').getContext('2d');
-    createChart(ctx, "Iluminación (LUX)", []);
+    createChart(ctx, "Iluminación (LUX)", dataLux);
     ctx = document.getElementById('potassiumChart').getContext('2d');
-    createChart(ctx, "Potasio (mg/l)", []);
+    createChart(ctx, "Potasio (mg/l)", dataPot);
     ctx = document.getElementById('sodiumChart').getContext('2d');
-    createChart(ctx, "Sodio (mg/l)", []);
+    createChart(ctx, "Sodio (mg/l)", dataSod);
     ctx = document.getElementById('calciumChart').getContext('2d');
-    createChart(ctx, "Calcio (mg/l)", []);
+    createChart(ctx, "Calcio (mg/l)", dataCal);
     ctx = document.getElementById('nitratesChart').getContext('2d');
-    createChart(ctx, "Nitratos (mg/l)", []);
+    createChart(ctx, "Nitratos (mg/l)", dataNitr);
 
 });
 
 
-function createChart(canvas, title, values) {
+function createChart(canvas, title, data) {
+    let colors = [];
+    let labels = [];
+    for(let index=0;index<30;index++){
+        labels.push(index);
+        colors.push('rgba(0, 0, 0, 1)');
+    }
     let myChart = new Chart(canvas, {
         type: 'line',
         data: {
-            labels: ['25-mar', '26-mar', '27-mar', '28-mar', '29-mar', '30-mar'],
+            labels: labels,
             datasets: [{
                 label: title,
-                data: [12, 19, 3, 5, 2, 3],
+                data: data,
                 backgroundColor: [
                     'rgba(1, 1, 1, 0)'
                 ],
-                borderColor: [
-                    'rgba(0, 0, 0, 1)',
-                    'rgba(0, 0, 0, 1)',
-                    'rgba(0, 0, 0, 1)',
-                    'rgba(0, 0, 0, 1)',
-                    'rgba(0, 0, 0, 1)',
-                    'rgba(0, 0, 0, 1)'
-                ],
+                borderColor: colors,
                 borderWidth: 1
             }]
         },
@@ -50,6 +60,75 @@ function createChart(canvas, title, values) {
 
                 }]
             }
+        }
+    });
+}
+
+function loadData() {
+    $.ajax({
+        url: "https://h5d6r9s3l8.execute-api.us-east-1.amazonaws.com/beta/measurement",
+        success: function (result) {
+            // Do something
+            let averageTemp, averageHum, averageCond, averageIllum;
+            let countTemp = 0, countHum = 0, countCond = 0, countIllum = 0;
+            for (let index = 0; index < result.length; ++index) {
+                countTemp += result[index]['payload']['temperature'];
+                countHum += result[index]['payload']['humidity'];
+                countCond += result[index]['payload']['conductivity'];
+                countIllum += result[index]['payload']['LUX'];
+                dataTemp.push(result[index]['payload']['temperature']);
+                dataHum.push(result[index]['payload']['humidity']);
+                dataCon.push(result[index]['payload']['conductivity']);
+                dataLux.push(result[index]['payload']['LUX']);
+            }
+
+            averageTemp = countTemp / result.length;
+            averageHum = countHum / result.length;
+            averageCond = countCond / result.length;
+            averageIllum = countIllum / result.length;
+
+            // Display elements
+            document.getElementById('avgTemp').innerHTML = averageTemp.toFixed(2) + " °C";
+            document.getElementById('avgHum').innerHTML = averageHum.toFixed(2) + "%";
+            document.getElementById('avgCond').innerHTML = averageCond.toFixed(2) + " S/cm";
+            document.getElementById('avgLUX').innerHTML = averageIllum.toFixed(2) + " LUX";
+        },
+        error: function (error) {
+            console.log("ERROR:" + JSON.stringify(error));
+        }
+    });
+}
+
+function loadMinerals() {
+    $.ajax({
+        url: "https://h5d6r9s3l8.execute-api.us-east-1.amazonaws.com/beta/minerals",
+        success: function (result) {
+            // Do something
+            let averagePot, averageSod, averageCal, averageNitr;
+            let countPot = 0, countSod = 0, countCal = 0, countNitr = 0;
+            for (let index = 0; index < result.length; ++index) {
+                countPot += result[index]['payload']['potassium'];
+                countSod += result[index]['payload']['sodium'];
+                countCal += result[index]['payload']['calcium'];
+                countNitr += result[index]['payload']['nitrates'];
+                dataPot.push(result[index]['payload']['potassium']);
+                dataSod.push(result[index]['payload']['sodium']);
+                dataCal.push(result[index]['payload']['calcium']);
+                dataNitr.push(result[index]['payload']['nitrates']);
+            }
+
+            averagePot = countPot / result.length;
+            averageSod = countSod / result.length;
+            averageCal = countCal / result.length;
+            averageNitr = countNitr / result.length;
+            // Display elements
+            document.getElementById('avgPot').innerHTML = averagePot.toFixed(2) + " mg/l";
+            document.getElementById('avgSod').innerHTML = averageSod.toFixed(2) + " mg/l";
+            document.getElementById('avgCal').innerHTML = averageCal.toFixed(2) + " mg/l";
+            document.getElementById('avgNitr').innerHTML = averageNitr.toFixed(2) + " mg/l";
+        },
+        error: function (error) {
+            console.log("ERROR:" + JSON.stringify(error));
         }
     });
 }
--- Frontend/scripts/functions.js
+++ Frontend/scripts/functions.js
@@ -1,20 +1,22 @@
-function login(username, password) {
+function login() {
+    let usuario = document.querySelector("#usrLog").value;
+    let contraseña = document.querySelector("#passLog").value;
     $.ajax({
         method: 'POST',
         url: "https://h5d6r9s3l8.execute-api.us-east-1.amazonaws.com/beta/login",
         data: {
-            username: username,
-            password: password
+            username: usuario,
+            password: contraseña
         },
-        success: function(token) {
+        success: function (token) {
             // Do something
             document.cookie = `token=${token};`;
             window.location('../dashboard.html');
         },
-        error: function(error) {
-            console.log("ERROR:"+JSON.stringify(error));
+        error: function (error) {
+            alert("ERROR al iniciar sesión. Intente nuevamente");
         }
-      });
+    });
 }
 
 function verify(token) {
@@ -22,31 +24,30 @@ function verify(token) {
         method: 'POST',
         url: "https://h5d6r9s3l8.execute-api.us-east-1.amazonaws.com/beta/login/verify",
         data: {
-            token: token 
+            token: token
         },
-        success: function(token) {
+        success: function (token) {
             // Do something if false
-            if(token['validation'] == false) {
+            if (token['validation'] == false) {
                 document.cookie = `token=`;
                 window.location('../index.html');
             }
         },
-        error: function(error) {
-            console.log("ERROR:"+JSON.stringify(error));
+        error: function (error) {
+            console.log("ERROR:" + JSON.stringify(error));
         }
-      });
+    });
 }
 
 
-function loadAverages() {    
+function loadAverages() {
     $.ajax({
         url: "https://h5d6r9s3l8.execute-api.us-east-1.amazonaws.com/beta/measurement",
-        success: function(result) {
+        success: function (result) {
             // Do something
-            let averageTemp, averageHum, averageCond,averageIllum;
-            let countTemp = 0, countHum = 0, countCond = 0,countIllum = 0;
+            let averageTemp, averageHum, averageCond, averageIllum;
+            let countTemp = 0, countHum = 0, countCond = 0, countIllum = 0;
             for (let index = 0; index < result.length; ++index) {
-                console.log(result[index]);
                 countTemp += result[index]['payload']['temperature'];
                 countHum += result[index]['payload']['humidity'];
                 countCond += result[index]['payload']['conductivity'];
@@ -59,14 +60,75 @@ function loadAverages() {
             averageIllum = countIllum / result.length;
 
             // Display elements
-            document.getElementById('avgTemp').innerHTML = averageTemp.toFixed(2) +" °C";
-            document.getElementById('avgHum').innerHTML = averageHum.toFixed(2) +"%";
-            document.getElementById('avgCond').innerHTML = averageCond.toFixed(2) +" S/cm";
-            document.getElementById('avgLUX').innerHTML = averageIllum.toFixed(2) +" LUX";
+            document.getElementById('avgTemp').innerHTML = averageTemp.toFixed(2) + " °C";
+            document.getElementById('avgHum').innerHTML = averageHum.toFixed(2) + "%";
+            document.getElementById('avgCond').innerHTML = averageCond.toFixed(2) + " S/cm";
+            document.getElementById('avgLUX').innerHTML = averageIllum.toFixed(2) + " LUX";
             document.getElementById('measurementsCount').innerHTML = result.length;
         },
-        error: function(error) {
-            console.log("ERROR:"+JSON.stringify(error));
+        error: function (error) {
+            console.log("ERROR:" + JSON.stringify(error));
         }
-      });
-  }
+    });
+}
+
+function manualMineral() {
+    console.log("Entrando :)");
+    let manualPotassium = document.querySelector('#manualPot').value;
+    let manualCalcium = document.querySelector('#manualCal').value;
+    let manualNitr = document.querySelector('#manualNit').value;
+    let manualSodium = document.querySelector('#manualSod').value;
+    let manualDates = document.querySelector('#date').value;
+    let manualInfo = {
+        deviceid: "nodemcu",
+        payload: {
+            calcium: manualCalcium,
+            nitrates: manualNitr,
+            potassium: manualPotassium,
+            sodium: manualSodium
+        }, date: manualDates
+    }
+
+    $.ajax({
+        method: 'POST', 
+        url: "https://h5d6r9s3l8.execute-api.us-east-1.amazonaws.com/beta/minerals",
+        data:manualInfo,
+        success: function (token) {
+            // Do something
+            alert("Guardado")
+        },
+        error: function (error) {
+            alert("Error en guardado, intente nuevamente")
+        }
+        
+    })
+}
+
+function register() {
+    let usuarioR = document.querySelector("#regUser").value;
+    let contraseñaR = document.querySelector("#regPass").value;
+    let emailR = document.querySelector("#regEmail").value;
+    let phoneR = document.querySelector("#regPhone").value;
+    let nameR = document.querySelector("#regName").value;
+
+    $.ajax({
+        method: 'POST',
+        url: "https://h5d6r9s3l8.execute-api.us-east-1.amazonaws.com/beta/login",
+        data: {
+            username: usuarioR,
+            password: contraseñaR,
+            email: emailR,
+            phone: phoneR,
+            name: nameR
+        },
+        success: function (token) {
+            // Do something
+            alert("Usuario registrado correctamente, favor de iniciar sesión")
+            document.cookie = `token=${token};`;
+            window.location('../index.html');
+        },
+        error: function (error) {
+            console.log("ERROR:" + JSON.stringify(error));
+        }
+    });
+}
